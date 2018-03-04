7dd1317c21be186a55ca7c4dfd56187f37a60594
Connor Weng <foamtea30@gmail.com>
2018-03-04 15:01:33 +0800

    update Launcher

--- component/PrimaryButton.js
+++ component/PrimaryButton.js
@@ -4,9 +4,25 @@ import { StyleSheet, Text, TouchableOpacity, } from 'react-native';
 export default class PrimaryButton extends Component {
   render() {
     return (
-      <TouchableOpacity onPress={this.props.onPress}>
-        <Text>{this.props.label}</Text>
+      <TouchableOpacity style={[styles.button, this.props.style]} onPress={this.props.onPress}>
+        <Text style={styles.buttonText}>{this.props.label}</Text>
       </TouchableOpacity>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  button: {
+    height: 42,
+    paddingLeft: 80,
+    paddingRight: 80,
+    borderRadius: 20,
+    backgroundColor: '#43BCD3',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 24,
+    color: '#ffffff',
+  },
+});
--- screen/Launcher.js
+++ screen/Launcher.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import PrimaryButton from '../component/PrimaryButton';
 
@@ -10,10 +10,72 @@ export default class Launcher extends Component {
 
   render() {
     return (
-      <SafeAreaView>
-        <Text>Launcher</Text>
-        <PrimaryButton label="Get Started" onPress={() => this.props.navigation.navigate('GetActive')}/>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Grow Together!</Text>
+        </View>
+        <View style={styles.imageContainer}>
+          <Image style={styles.biking} source={require('../resource/biking.png')}/>
+        </View>
+        <View style={styles.footerContainer}>
+          <PrimaryButton style={styles.button} label="Get Started" onPress={() => this.props.navigation.navigate('GetActive')}/>
+            <View style={styles.copyrightsContainer}>
+              <Text style={styles.copyrights}>By continuing, you agree to our </Text>
+              <Text style={styles.terms}>Terms of Use & Privacy Policy</Text>
+            </View>
+        </View>
       </SafeAreaView>
     );
   }
 }
+
+const { height, width } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  titleContainer: {
+    flex: 1,
+  },
+  imageContainer: {
+    flex: 7,
+    flexDirection: 'column',
+    height: height/2,
+  },
+  buttonContainer: {
+    flex: 1,
+    width: width,
+  },
+  footerContainer: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: width,
+  },
+  button: {
+    justifyContent: 'center',
+  },
+  biking: {
+    width: width * 1.2,
+    resizeMode: Image.resizeMode.contain,
+  },
+  title: {
+    fontSize: 28,
+    paddingTop: 40,
+  },
+  copyrightsContainer: {
+    flexDirection: 'row'
+  },
+  copyrights: {
+    fontSize: 10,
+    color: 'rgba(0, 0, 0, 0.2)'
+  },
+  terms: {
+    fontSize: 10,
+    color: 'rgba(0, 0, 0, 0.5)'
+  },
+});
